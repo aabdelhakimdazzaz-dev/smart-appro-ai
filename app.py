422f6d1c868d384f3bd7eafd2bd275ac25002d04
aabdelhakimdazzaz-dev <aabdelhakimdazzaz@gmail.com>
2026-04-26 23:37:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,19 +2,20 @@ import streamlit as st
 
 st.set_page_config(page_title="Smart Appro AI", layout="wide")
 st.title("🚚 Smart Appro AI - PFE 2025")
-st.success("✅ Deploy Naja7 bla Pillow!")
+st.success("✅ Deploy Naja7 f Python 3.14!")
 
-st.header("📊 KPI")
+st.header("📊 KPI Retards Fournisseurs")
 c1, c2, c3 = st.columns(3)
-c1.metric("Commandes Risque", "2 🔴")
-c2.metric("Perte Potentielle", "45,000 MAD") 
-c3.metric("Accuracy IA", "91.3%")
+c1.metric("Commandes à Risque Élevé", "2 🔴")
+c2.metric("Perte Potentielle Évitable", "45,000 MAD") 
+c3.metric("Accuracy Modèle IA", "91.3%")
 
-st.header("📋 Data")
-st.write({
-    'ID_Cmd': [45001, 45003], 
-    'Fournisseur': ['F102', 'F203'], 
-    'Risque_%': [87, 65]
-})
+st.header("📋 Données Exemple")
+data = [
+    {"ID_Cmd": 45001, "Fournisseur": "F102", "Pays": "Chine", "Risque_%": 87},
+    {"ID_Cmd": 45003, "Fournisseur": "F203", "Pays": "Turquie", "Risque_%": 65},
+    {"ID_Cmd": 45006, "Fournisseur": "F405", "Pays": "Italie", "Risque_%": 45}
+]
+st.table(data)
 
-st.info("Version 0.1: Base khdama. Nzado pandas mn ba3d.")
+st.info("Version 0.2: Streamlit 1.39 + Protobuf 5.28 khdamin. Nzido Pandas mn ba3d.")
